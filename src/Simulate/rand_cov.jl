e8b35e023cff99f6f1d0e60d823a098ef25d29da
Aaron Peikert <aaron.peikert@posteo.de>
2023-04-18 16:51:38 +0100

    force symmetry in `rand_cov`

--- src/Simulate/rand_cov.jl
+++ src/Simulate/rand_cov.jl
@@ -1,5 +1,5 @@
 import Random
-import LinearAlgebra: eigen, diagm
+import LinearAlgebra: eigen, diagm, Symmetric
 export rand_cov
 """
     rand_cov(k::Int, [rng = Random.GLOBAL_RNG])
@@ -10,7 +10,7 @@ Generate a random covariance matrix of size `k×k`.
 julia> using TDLM.Simulate
 
 julia> rand_cov(3, StableRNGs.StableRNG(42))
-3×3 Matrix{Float64}:
+3×3 LinearAlgebra.Symmetric{Float64, Matrix{Float64}}:
   1.39477     0.156631   -0.0456395
   0.156631    1.54293    -0.0163959
  -0.0456395  -0.0163959   0.797937
@@ -20,5 +20,5 @@ julia> rand_cov(3, StableRNGs.StableRNG(42))
 function rand_cov(k::Int, rng = Random.GLOBAL_RNG)
     A = randn(rng, k, k)
     _, U = eigen((A+A')/2)
-    U*diagm(abs.(randn(rng, k)))*U'
+    Symmetric(U*diagm(abs.(randn(rng, k)))*U')
 end
