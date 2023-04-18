8cbd39fb4d188a1a3631f26c47d40b89b4104551
Documenter.jl <documenter@juliadocs.github.io>
2023-04-18 09:18:53 +0000

    build based on 5c297c9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TDLM","category":"page"},{"location":"#TDLM","page":"Home","title":"TDLM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TDLM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TDLM]","category":"page"}]
+}
