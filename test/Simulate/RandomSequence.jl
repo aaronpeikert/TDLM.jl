828d5528513d480fdd755a2c12bd4538d95d42ba
Aaron Peikert <aaron.peikert@posteo.de>
2023-04-19 10:16:55 +0100

    add basic implementation of RandomSequence

---
+++ test/Simulate/RandomSequence.jl
@@ -0,0 +1,15 @@
+import Distributions
+using TDLM
+using TDLM.Simulate
+
+@testset "RandomSequence" begin
+    d1 = Distributions.MvNormal(rand_cov(5))
+    d2 = 1:5
+    s1 = RandomSequence(d1)
+    s2 = RandomSequence(d2)
+    @test size(s1) == (5, Inf)
+    @test eltype(s1) == Vector{eltype(d1)}
+    @test size(s2) == (1, Inf)
+    @test eltype(s2) == Vector{eltype(d2)}
+    @test size(s1[1:5, :]) == (5, 5)
+end
