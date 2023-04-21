27c3184bee540ec4961c08cc37920d76579666e8
Aaron Peikert <aaron.peikert@posteo.de>
2023-04-21 17:46:33 +0100

    only one parameter really needed

--- src/Simulate/RandomSequence.jl
+++ src/Simulate/RandomSequence.jl
@@ -22,24 +22,24 @@ If no function is is given, they will simply be added.
 ```jldoctest; setup = :(using TDLM.Simulate, StableRNGs)
 julia> import Distributions
 
-julia> s1 = RandomSequence(Distributions.MvNormal(rand_cov(2)); rng = StableRNG(42));
+julia> s1 = RandomSequence(Distributions.MvNormal(rand_cov(2, rng = StableRNG(42))); rng = StableRNG(42));
 
 julia> s1[1:100, :] # sequence is infinite, simply index as much as you need
 100×2 Matrix{Float64}:
- -0.589181   0.291201
-  0.618301   1.4553
-  0.729122   2.00813
- -0.166802   1.28532
-  1.39326    2.47718
-  ⋮         
- -6.539     -0.143812
- -6.87956   -1.17514
- -8.02183   -2.12672
- -8.22977   -1.72463
+ -0.299517    0.0105985
+  0.314321    1.45151
+  0.370658    1.95421
+ -0.0847956   1.00213
+  0.708281    2.60451
+  ⋮          
+ -3.32418    -2.6863
+ -3.49731    -3.67663
+ -4.07799    -4.91543
+ -4.1837     -4.66306
 
 julia> # dist can also be a vector, and mix can be an abitrary function
 
-julia> RandomSequence([2 4], (prev, next) -> next - prev; rng = StableRNG(43))[1:5, :]
+julia> RandomSequence([2 4], mix = (prev, next) -> next - prev, rng = StableRNG(43))[1:5, :]
 5×1 Matrix{Int64}:
  4
  0
@@ -48,20 +48,20 @@ julia> RandomSequence([2 4], (prev, next) -> next - prev; rng = StableRNG(43))[1
  2
 ```
 """
-struct RandomSequence{T1, T2}
-    dist::T1
-    mix::T2
+struct RandomSequence{T}
+    dist::T
+    mix::Function
     rng
-    function RandomSequence(dist::T1, mix::T2 = default_mix; rng = Random.GLOBAL_RNG) where {T1 <: Union{AbstractArray, Sampleable}, T2 <: Function}
-        new{T1, T2}(dist, mix, rng)
+    function RandomSequence(dist::T; mix = default_mix, rng = Random.GLOBAL_RNG) where {T <: Union{AbstractArray, Sampleable}}
+        new{T}(dist, mix, rng)
     end
 end
 function Base.iterate(S::RandomSequence, prev = zeros(eltype(eltype(S)), size(S, 1)))
     state = vec(S.mix(prev, rand(S.rng, S.dist, 1)))
     (state, state)
 end
-Base.IteratorSize(::Type{RandomSequence{T1, T2}}) where {T1, T2} = Base.IsInfinite()
-Base.eltype(::Type{RandomSequence{T1, T2}}) where {T1, T2} = Vector{eltype(T1)}
+Base.IteratorSize(::Type{RandomSequence{T}}) where {T} = Base.IsInfinite()
+Base.eltype(::Type{RandomSequence{T}}) where {T} = Vector{eltype(T)}
 Base.size(S::RandomSequence{T}) where {T <: Sampleable} = (length(S.dist), Inf)
 Base.size(::RandomSequence{T}) where {T <: AbstractArray} = (1, Inf)
 Base.size(S::RandomSequence, d::Integer) = size(S)[convert(Int, d)]
