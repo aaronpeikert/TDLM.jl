539ac47f62ad5c02d9ea725ddc5ce2a2c77b839d
Aaron Peikert <aaron.peikert@posteo.de>
2023-04-21 17:54:05 +0100

    dont explicitly load

--- src/Simulate/rand_cov.jl
+++ src/Simulate/rand_cov.jl
@@ -6,9 +6,7 @@ export rand_cov
 
 Generate a random covariance matrix of size `k×k`.
 
-```jldoctest; setup = :(using StableRNGs)
-julia> using TDLM.Simulate
-
+```jldoctest; setup = :(using TDLM.Simulate, StableRNGs)
 julia> rand_cov(3, rng = StableRNG(42))
 3×3 LinearAlgebra.Symmetric{Float64, Matrix{Float64}}:
   1.39477     0.156631   -0.0456395
