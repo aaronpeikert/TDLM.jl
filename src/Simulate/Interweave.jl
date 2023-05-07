2a5ac96b2d79655fb4e907ae4ce0dcab672c7fbe
Aaron Peikert <aaron.peikert@posteo.de>
2023-05-07 18:27:17 +0100

    fix a couple of mistakes in implementation and documentation

--- src/Simulate/Interweave.jl
+++ src/Simulate/Interweave.jl
@@ -1,7 +1,8 @@
 """
-    interweave(is...; ns = missing)
+    interweave(is...; limiters = 1)
 
 Interweave takes a number of iterators and returns them alternating till one runs out of elements.
+By default it returns one element per iterator, but limimters accept (a) function(s) that controls when alternation occurs.
 
 ```jldoctest; setup = :(using TDLM.Simulate)
 julia> collect(interweave(1:2, 11:12))
@@ -11,7 +12,7 @@ julia> collect(interweave(1:2, 11:12))
   2
  12
 
- julia> collect(interweave(1:3, Iterators.cycle(0), ns = (1, 2)))
+ julia> collect(interweave(1:3, Iterators.cycle(0), limiters = (1, 2)))
  9-element Vector{Int64}:
   1
   0
@@ -22,70 +23,67 @@ julia> collect(interweave(1:2, 11:12))
   3
   0
   0
+
+julia> collect(interweave(1:3, 11:13, limiters = x -> random_length(x, [1 2])));
 ```
 """
-interweave(is...; ns = missing) = Interweave(is; ns)
+interweave(is...; limiters = 1) = Interweave(is, limiters)
 
-struct Interweave{Is}
+struct Interweave{Is, K}
     is::Is
-    ns::Tuple
-    function Interweave(is::T; ns = missing) where T
-        ns = ismissing(ns) ? ones_for_tuple(T) : ns
-        length(ns) != length(is) && throw(ArgumentError("`ns` must be the same length as `is`."))
-        new{T}(is, ns)
+    limiters::NTuple{K, Function}
+    function Interweave(is::Is, limiters::NTuple{K, Function}) where {Is, K}
+        K != length(is) && throw(ArgumentError("`limiters` must be the same length as `is`."))
+        new{Is, K}(is, limiters)
     end
 end
-Interweave(is...; ns = missing) = Interweave(is; ns = ns)
+function Interweave(is, limiter::Function)
+    Interweave(is, tuple_repeat_along(is, limiter))
+end
+function Interweave(is, limiter::Int)
+    Interweave(is, x -> Iterators.take(x, limiter))
+end
+function Interweave(is, limiters::Union{Tuple{Vararg{T}}, AbstractVector{<:T}}) where T <: Int
+    Interweave(is, tuple((x -> Iterators.take(x, l) for l in limiters)...))
+end
 
 tuple_length(::Type{<:NTuple{N, Any}}) where {N} = Val{N}()
 tuple_length(x) = tuple_length(typeof(x))
-ones_for_tuple(x::Type{<:NTuple{N, Any}}) where {N} = ntuple(_ -> 1, tuple_length(x))
-ones_for_tuple(x) = ones_for_tuple(typeof(x))
+tuple_repeat_along(x::T, t) where {T<: Tuple} = ntuple(_ -> t, tuple_length(T))
 
 struct InterweaveState
     outer
     outer_bundle
-    ns
-    ns_bundle
     inner
     inner_state
 end
 
-function Base.iterate(s::Interweave, state::InterweaveState = InterweaveState(missing, missing, missing, missing, missing, missing))
-    if ismissing(state.outer_bundle)
-        outer = Iterators.cycle(map(Iterators.Stateful, s.is))
-        outer_bundle = iterate(outer)
-        ns = Iterators.cycle(s.ns)
-        ns_bundle = iterate(ns)
-    else
-        outer = state.outer
-        outer_bundle = state.outer_bundle
-        ns = state.ns
-        ns_bundle = state.ns_bundle
-    end
+function Base.iterate(s::Interweave)
+    is = Iterators.cycle(map(Iterators.Stateful, s.is))
+    limiters = Iterators.cycle(s.limiters)
+    outer = zip(is, limiters)
+    outer_bundle = iterate(outer)
+    iterate(s, InterweaveState(outer, outer_bundle, missing, missing))
+end
 
+function Base.iterate(s::Interweave, state::InterweaveState)
     if ismissing(state.inner_state)
-        if isinf(ns_bundle[1])
-            inner = outer_bundle[1]
-        else
-            inner = Iterators.take(outer_bundle[1], ns_bundle[1])
-        end
+        inner = state.outer_bundle[1][2](state.outer_bundle[1][1])
         inner_bundle = iterate(inner)
         inner_bundle === nothing && return nothing
-        return inner_bundle[1], InterweaveState(outer, outer_bundle, ns, ns_bundle, inner, inner_bundle[2])
+        return inner_bundle[1], InterweaveState(state.outer, state.outer_bundle, inner, inner_bundle[2])
     end
 
     inner_bundle = iterate(state.inner, state.inner_state)
 
     if inner_bundle === nothing
-        outer_bundle = iterate(outer, outer_bundle[2])
-        ns_bundle = iterate(ns, ns_bundle[2])
-        return iterate(s, InterweaveState(outer, outer_bundle, ns, ns_bundle, missing, missing))
+        outer_bundle = iterate(state.outer, state.outer_bundle[2])
+        return iterate(s, InterweaveState(state.outer, outer_bundle, missing, missing))
     else
-        return inner_bundle[1], InterweaveState(state.outer, state.outer_bundle, state.ns, state.ns_bundle, state.inner, inner_bundle[2])
+        return inner_bundle[1], InterweaveState(state.outer, state.outer_bundle, state.inner, inner_bundle[2])
     end
 end
-Base.IteratorSize(::Type{Interweave{T}}) where {T} = Base.SizeUnknown()
-function Base.eltype(::Type{Interweave{T}}) where {T}
-    promote_type(map(eltype, fieldtypes(T))...)
+Base.IteratorSize(::Type{Interweave{T1, T2}}) where {T1,T2} = Base.SizeUnknown()
+function Base.eltype(::Type{Interweave{T1, T2}}) where {T1,T2}
+    promote_type(map(eltype, fieldtypes(T1))...)
 end
